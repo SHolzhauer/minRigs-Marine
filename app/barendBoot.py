2872571053f13b2d4e5d372c1cce85de61c56d25
Stijn Holzhauer <stijn.holzhauer@randstadgroep.nl>
2024-10-29 20:18:18 +0100

    disabling dashboard

--- app/barendBoot.py
+++ app/barendBoot.py
@@ -5,7 +5,7 @@ from kivy.uix.label import Label
 from kivy.core.window import Window  # Import the Window module
 from base import Logboek, startup, shutdown
 from power_management import PowerManagement    # Import the PowerManagement class
-from dashboard import Dashboard                 # Import the Dashboard class
+#from dashboard import Dashboard                 # Import the Dashboard class
 from logboek import LogboekDisplay              # Import the LogboekDisplay class
 
 logboek = Logboek()
@@ -26,8 +26,8 @@ class MyApp(App):
         accordion = Accordion(orientation='horizontal')
 
         # Dashboard
-        dashboard_item = AccordionItem(title='Dashboard')
-        dashboard_item.add_widget(Dashboard())
+        #dashboard_item = AccordionItem(title='Dashboard')
+        #dashboard_item.add_widget(Dashboard())
         
         # Power Management
         power_mgmt_item = AccordionItem(title='Power Management')
@@ -41,7 +41,7 @@ class MyApp(App):
         # Add accordion items to accordion
         accordion.add_widget(logboek_item)
         accordion.add_widget(power_mgmt_item)
-        accordion.add_widget(dashboard_item)
+        #accordion.add_widget(dashboard_item)
 
         # Add accordion to the main layout
         layout.add_widget(accordion)
