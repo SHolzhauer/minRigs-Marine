f7f15b5d00cedb2049ae7b52be31d82f11097637
Stijn Holzhauer <stijn.holzhauer@randstadgroep.nl>
2024-10-30 08:50:54 +0100

    wip

--- app.sh
+++ app.sh
@@ -3,4 +3,6 @@
 git pull
 source /opt/minRigs-Marine/venv/bin/activate
 pip install -r requirements.txt
+chmod +x venv/bin/garden
+garden install mapview
 python /opt/minRigs-Marine/app/barendBoot.py
--- app/navigation.py
+++ app/navigation.py
@@ -2,13 +2,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.clock import Clock
-from kivy.garden.mapview import MapView, MarkerMap
+from kivy_garden.mapview import MapView, MarkerMap
 
 
 class NauticalMap(BoxLayout):
+    
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        map = MapView()
+        map = MapView(zoom=13, lat=52.30, lon=4.47)
 
     def update_webview(self):
         # Continuously update the content in the WebKit widget
