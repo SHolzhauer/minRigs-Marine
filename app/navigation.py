724d9bca78d6933f3e46098bd36eae40bb408cf7
Stijn Holzhauer <stijn.holzhauer@randstadgroep.nl>
2024-10-30 08:38:21 +0100

    mapview

--- app/navigation.py
+++ app/navigation.py
@@ -1,31 +1,14 @@
-import gi
-gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
-from gi.repository import Gtk, WebKit2
-import webview
-
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.clock import Clock
-
-from threading import Thread
+from kivy.garden.mapview import MapView, MarkerMap
 
 
 class NauticalMap(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.webview = WebKit2.WebView()
-        self.webview.load_uri("https://www.google.com/maps")
-
-        # Create a GTK window off-screen to hold the webview
-        self.window = Gtk.Window()
-        self.window.set_size_request(800, 600)
-        self.window.add(self.webview)
-        self.window.show_all()
-
-        # Render the webview content within Kivy (simplified)
-        Thread(target=self.update_webview).start()
+        map = MapView()
 
     def update_webview(self):
         # Continuously update the content in the WebKit widget
