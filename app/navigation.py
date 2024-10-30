4a0b0c93010a096d45e1d3f2da5d639d12e9e1b2
Stijn Holzhauer <stijn.holzhauer@randstadgroep.nl>
2024-10-30 09:28:49 +0100

    some tuning

--- app/navigation.py
+++ app/navigation.py
@@ -2,28 +2,28 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle, Color
 from kivy.clock import Clock
-from kivy_garden.mapview import MapView
+from kivy_garden.mapview import MapView, MapMarker, MapSource
 
 
 class NauticalMap(BoxLayout):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.map = MapView(zoom=13, lat=52.51677023841208, lon=4.788266954784756)
-
-        # Define min and max zoom limits
-        self.min_zoom = 10
-        self.max_zoom = 17
+        
+        mapsource = MapSource(
+            min_zoom=13,
+            max_zoom=16
+        )
+        
+        self.map = MapView(
+            map_source=mapsource,
+            zoom=13, 
+            lat=52.51677023841208, 
+            lon=4.788266954784756,
+            pause_on_action=False
+        )
+        
         
         # Add map widget to layout
         self.add_widget(self.map)
-        
-        # Schedule a check to enforce zoom limits on every frame
-        Clock.schedule_interval(self.enforce_zoom_limits, 1 / 60)
 
-    def enforce_zoom_limits(self, dt):
-        # Adjust zoom to stay within bounds if it exceeds limits
-        if self.map.zoom < self.min_zoom:
-            self.map.zoom = self.min_zoom
-        elif self.map.zoom > self.max_zoom:
-            self.map.zoom = self.max_zoom
